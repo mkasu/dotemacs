ae46372d8197fd107a88e031739d759a77044467
Marc A. Kastner <mkastner@fastmail.fm>
2015-11-08 16:46:43 +0100

    Per-project org-mode

--- config/30-org.el
+++ config/30-org.el
@@ -7,12 +7,14 @@
 ;;; Code:
 
 (use-package org
+  :ensure t
   :init
   (add-hook 'org-mode-hook 'visual-line-mode)
   (add-hook 'org-mode-hook 'org-indent-mode)
   (add-hook 'org-mode-hook 'flyspell-mode)
   (setq org-agenda-window-setup 'current-window)
   (setq org-agenda-files (quote ("~/org")))
+  (setq org-log-done 'time)
   ; Run/highlight code using babel in org-mode
   (org-babel-do-load-languages
    'org-babel-load-languages
@@ -24,5 +26,18 @@
   (setq org-src-fontify-natively t)
   :defer t
   )
+
+(use-package org-projectile
+  :bind (("C-c n p" . org-projectile:project-todo-completing-read)
+         ("C-c n c" . org-capture))
+  :config
+  (progn
+    (setq org-projectile:projects-file 
+          "~/org/projects.org")
+    (setq org-agenda-files (append org-agenda-files (org-projectile:todo-files)))
+    (add-to-list 'org-capture-templates (org-projectile:project-todo-entry "p"))
+    (add-to-list 'org-capture-templates (org-projectile:project-todo-entry "l" "* TODO %? %a\n" "Linked Project TODO"))
+    )
+  :ensure t)
 
 ;;; 30-org.el ends here
