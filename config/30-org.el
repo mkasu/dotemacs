c238fa4be34733e2ad682c19c522e9071407fe95
Marc A. Kastner <mkastner@fastmail.fm>
2015-11-11 16:19:30 +0100

    Added org-agenda customizations.

--- config/30-org.el
+++ config/30-org.el
@@ -6,16 +6,43 @@
 
 ;;; Code:
 
+(require 'use-package)
+
+(defun sacha/org-agenda-skip-scheduled ()
+  (org-agenda-skip-entry-if 'scheduled 'deadline 'regexp "\n]+>"))
+
 (use-package org
   :ensure t
+  :bind ("C-c a" . org-agenda)
   :init
   (add-hook 'org-mode-hook 'visual-line-mode)
   (add-hook 'org-mode-hook 'org-indent-mode)
   (add-hook 'org-mode-hook 'flyspell-mode)
+  ;; Agenda
   (setq org-agenda-window-setup 'current-window)
+  (setq org-agenda-custom-commands
+        '(("h" "Main view"
+           ((agenda "" ((org-agenda-ndays 7)                      ;; overview of appointments
+                        (org-agenda-log-mode 1)
+                        (org-agenda-start-on-weekday nil)         ;; calendar begins today
+                        (org-agenda-repeating-timestamp-show-all t)
+                        )
+                    )         
+           (alltodo ""
+                    ((org-agenda-skip-function 'sacha/org-agenda-skip-scheduled)
+                     (org-agenda-overriding-header "Unscheduled TODO entries: ")
+                     )
+                    )
+           )
+           )
+          )
+        )
   (setq org-agenda-files (quote ("~/org")))
   (setq org-log-done 'time)
-  ; Run/highlight code using babel in org-mode
+  (setq org-agenda-skip-deadline-if-done t)
+  (setq org-agenda-skip-scheduled-if-done t)
+  (setq org-agenda-skip-scheduled-if-deadline-is-shown t)
+  ;; Run/highlight code using babel in org-mode
   (org-babel-do-load-languages
    'org-babel-load-languages
    '(
