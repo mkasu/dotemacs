e6ec32e068fac65b5111eb132a720ada20be10b5
Marc A. Kastner <mkastner@fastmail.fm>
2015-11-13 01:09:18 +0100

    Added org-capture templates. Fixed org-projectile.

--- config/30-org.el
+++ config/30-org.el
@@ -11,10 +11,20 @@
 (defun sacha/org-agenda-skip-scheduled ()
   (org-agenda-skip-entry-if 'scheduled 'deadline 'regexp "\n]+>"))
 
+(defvar my/org-basic-task-template "* TODO %^{Task}
+SCHEDULED: %^t
+%<%Y-%m-%d %H:%M>
+%?
+" "Basic task data")
+
+
+
+
 (use-package org
   :ensure t
   :bind ("C-c a" . org-agenda)
   :init
+  (progn
   (add-hook 'org-mode-hook 'visual-line-mode)
   (add-hook 'org-mode-hook 'org-indent-mode)
   (add-hook 'org-mode-hook 'flyspell-mode)
@@ -28,12 +38,12 @@
                         (org-agenda-repeating-timestamp-show-all t)
                         )
                     )         
-           (alltodo ""
-                    ((org-agenda-skip-function 'sacha/org-agenda-skip-scheduled)
-                     (org-agenda-overriding-header "Unscheduled TODO entries: ")
+            (alltodo ""
+                     ((org-agenda-skip-function 'sacha/org-agenda-skip-scheduled)
+                      (org-agenda-overriding-header "Unscheduled TODO entries: ")
+                      )
                      )
-                    )
-           )
+            )
            )
           )
         )
@@ -51,20 +61,45 @@
      ))
   ;; Syntax hilight in #+begin_src blocks
   (setq org-src-fontify-natively t)
-  :defer t
-  )
+  ;; Capturing
+  (setq org-capture-templates
+        '(
+          ("t" "Tasks" entry
+           (file+headline "~/Dropbox/org/todo.org" "Inbox")
+           ,my/org-basic-task-template)
+          ("T" "Quick task" entry
+           (file+headline "~/Dropbox/org/todo.org" "Inbox")
+           "* TODO %^{Task}"
+           :immediate-finish t)
+          ("B" "Book" entry
+           (file+headline "~/Dropbox/org/books.org" "Inbox")
+           "* %^{Title}  %^g
+%i
+*Author(s):* %^{Author} \\\\
 
-(use-package org-projectile
-  :bind (("C-c n p" . org-projectile:project-todo-completing-read)
-         ("C-c n c" . org-capture))
-  :config
-  (progn
-    (setq org-projectile:projects-file 
-          "~/Dropbox/org/projects.org")
-    (setq org-agenda-files (append org-agenda-files (org-projectile:todo-files)))
+%?
+
+%U"
+           )
+          ("j" "Japanese Grammar" entry
+           (file+headline "~/Dropbox/org/japanese.org" "Grammar")
+           "* %^{Title}
+%?"
+           )
+          )
+        )
+  ;; Org Projectile
+  (use-package org-projectile
+    :bind (("C-c n p" . org-projectile:project-todo-completing-read)
+           ("C-c n c" . org-capture))
+    :ensure t
+    :demand t
+    :config
+    (setq org-projectile:projects-file "~/Dropbox/org/projects.org")
     (add-to-list 'org-capture-templates (org-projectile:project-todo-entry "p"))
-    (add-to-list 'org-capture-templates (org-projectile:project-todo-entry "l" "* TODO %? %a\n" "Linked Project TODO"))
+    (add-to-list 'org-capture-templates (org-projectile:project-todo-entry "l" "* TODO %? %a\n" "Linked Project TODO"))   
     )
-  :ensure t)
+  )
+)
 
 ;;; 30-org.el ends here
