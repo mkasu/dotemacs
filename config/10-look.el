a1f3ac7d6ea70dbb8ec83193d45f7991f3f0d017
Marc A. Kastner <mkastner@fastmail.fm>
2015-11-14 21:26:44 +0100

    Force new font for all Japanese fontsets.

--- config/10-look.el
+++ config/10-look.el
@@ -18,6 +18,16 @@
                     :weight 'normal
                     :width 'normal)
 
+(set-fontset-font (frame-parameter nil 'font)
+                  'japanese-jisx0208
+                  (cons "Ricty Diminished" "iso10646-1"))
+(set-fontset-font (frame-parameter nil 'font)
+                  'japanese-jisx0212
+                  (cons "Ricty Diminished" "iso10646-1"))
+(set-fontset-font (frame-parameter nil 'font)
+                  'katakana-jisx0201
+                  (cons "Ricty Diminished" "iso10646-1"))
+
 ;; Line Numbers
 (global-linum-mode t)
 
