0dff9fb543ad50b49d6f84083951729d50f9ac72
Marc A. Kastner <mkastner@fastmail.fm>
2015-11-19 14:46:35 +0100

    Updated Apple reminder script.
    We don't need rem.py anymore. I pushed all org-mode relevant changes
    upstream to be added to rem. Currently my org-mode compatible version is
    in mkasu/rem on Github.

--- scripts/rem.sh
+++ scripts/rem.sh
@@ -4,7 +4,8 @@ SRC=/tmp/rem.org
 DEST=~/Dropbox/org/ios.org
 
 echo "# -*- coding: utf-8; eval: (auto-revert-mode 1); -*-" > ${SRC}
-~/.emacs.d/scripts/rem.py >> ${SRC}
+#~/.emacs.d/scripts/rem.py >> ${SRC}
+~/.emacs.d/scripts/rem orgmode ${SRC}
 
 # check if it changed
 if ! cmp ${SRC} ${DEST} > /dev/null 2>&1
