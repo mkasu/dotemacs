6ec19434e8aaab85563e37db9dd32e978739fa34
Marc A. Kastner <mkastner@fastmail.fm>
2015-11-25 18:52:21 +0100

    Added org-babel config and some agenda options.

--- config/30-org.el
+++ config/30-org.el
@@ -1,3 +1,4 @@
+
 ;;; 30-org.el --- Org Mode
 
 ;;; Commentary:
@@ -8,6 +9,8 @@
 
 (require 'use-package)
 
+
+
 (use-package org
   :ensure t
   :bind ("C-c a" . org-agenda)
@@ -16,6 +19,8 @@
     (add-hook 'org-mode-hook 'visual-line-mode)
     (add-hook 'org-mode-hook 'org-indent-mode)
     (add-hook 'org-mode-hook 'flyspell-mode)
+    (dolist (hook '(change-log-mode-hook log-edit-mode-hook))
+      (add-hook hook (lambda () (flycheck-mode -1))))
     ;; Agenda
     (setq org-agenda-window-setup 'current-window)
     
@@ -48,7 +53,8 @@
                           (org-agenda-log-mode 1)
                           (org-agenda-start-on-weekday nil)         ;; calendar begins today
                           (org-agenda-repeating-timestamp-show-all t)
-                          (org-agenda-use-time-grid t)))       
+                          (org-agenda-use-time-grid t))
+                      )       
               (alltodo ""
                        ((org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled 'deadline 'regexp "\n]+>"))
                         (org-agenda-overriding-header "Unscheduled TODO entries:")
@@ -63,8 +69,11 @@
     (setq org-agenda-skip-deadline-if-done t)
     (setq org-agenda-skip-scheduled-if-done t)
     (setq org-agenda-skip-scheduled-if-deadline-is-shown t)
-    (setq org-support-shift-select t)
+    (setq org-track-ordered-property-with-tag t)
+    (setq org-enforce-todo-dependencies t)
+    (setq org-agenda-dim-blocked-tasks 'invisible)
     (setq org-agenda-show-inherited-tags 'nil)
+    (setq org-support-shift-select t)
     (setq org-publish-project-alist
           '(("html"
              :base-directory "~/Dropbox/org/"
@@ -125,14 +134,11 @@ SCHEDULED: %^t
 
 %U"
              )
-            ("j" "Japanese Grammar" entry
-             (file+headline "~/Dropbox/org/japanese.org" "Grammar")
-             "* %^{Title}
-
+            ("e" "Email Task" entry
+             (file+headline "~/Dropbox/org/todo.org" "Email")
+             "* TODO %^{Title}
+%a
 %?
-
-例文
-- 
 "
              )
             )
@@ -165,6 +171,35 @@ SCHEDULED: %^t
       (setq calendar-week-start-day 1))
     
     )
+
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+     (gnuplot . t)
+     (latex . t)
+     (ledger . t) 
+     (ocaml . nil)
+     (python . t)
+     (ruby . t)
+     (screen . nil)
+     (sh . t)
+     (sql . nil)
+     (sqlite . t)))
+  
+  (setq org-confirm-babel-evaluate nil)
+
+  (defun org-babel-remove-result-buffer ()
+  "Remove results from every code block in buffer."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward org-babel-src-block-regexp nil t)
+      (org-babel-remove-result))))
+  (global-set-key (kbd "C-c C-v C-k") 'org-babel-remove-result-buffer)
+
+  (use-package org-depend
+    )
+  
   )
 
 (eval-after-load 'ox ;; shouldn't be byte compiled.
