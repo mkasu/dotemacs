0f76077acc2f673aad25ccae0e23b0c00cb25329
Marc A. Kastner <mkastner@fastmail.fm>
2016-12-25 01:14:49 +0900

    Automatic commit at Sun Dec 25 01:14:49 JST 2016

--- scripts/agenda-ical.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Cron job script for exporting org-mode stuff in background
-/usr/local/bin/emacs --batch --user mkastner --eval "(org-icalendar-combine-agenda-files)"
-
--- scripts/rem_parse.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-SRC=~/Dropbox/org/ios.org
-
-/usr/local/bin/emacs --batch --user mkastner --eval "(cli-org-export-json)" ${SRC} &> ~/.emacs.d/scripts/rem.log
-~/.emacs.d/scripts/rem parseorg ${SRC}.json >> ~/.emacs.d/scripts/rem.log 2>&1
-rm ${SRC}.json
