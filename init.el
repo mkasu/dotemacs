f9cd818e34669a35da79285f096d8c7ef3fb1b89
Marc A. Kastner <mkastner@fastmail.fm>
2017-01-25 16:46:16 +0900

    Revert to 9db91bd708a47ae52288595d593ebfeebbf5d08d

--- init.el
+++ init.el
@@ -3,20 +3,45 @@
 ;;; Commentary:
 ;;; Personal config
 
-;; (add-to-list 'load-path "/home1/kastnerm/.emacs.d/elpa/benchmark-init-20150905.238/")
-;; (require 'benchmark-init)
-;; (benchmark-init/activate)
+;;; Needs to come first so the package-archives customization gets
+;;; picked up.
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 
-(require 'org)
-;; Open the configuration
-(find-file (concat user-emacs-directory "init.org"))
-;; tangle it
-(org-babel-tangle)
-;; load it
-(load-file (concat user-emacs-directory "init.el"))
-;; finally byte-compile it
-(byte-compile-file (concat user-emacs-directory "init.el"))
+;; This file contains encryption keys etc. and won't be uploaded to repositories.
+(setq secret-file "~/.emacs.d/secret.el")
+
+(when (file-exists-p secret-file)
+    (load secret-file))
+
+;; User Info
+(setq user-full-name "Marc A. Kastner")
+(setq user-mail-address "marc@mkasu.org")
 
 ;;; Code:
+(require 'package
+    (setq package-enable-at-startup nil)
+    (setq package-check-signature nil))
+
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+; activate all the packages (in particular autoloads)
+(package-initialize)
+
+; fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "settings.org"
+                   user-emacs-directory))
 
 ;;; init.el ends here
