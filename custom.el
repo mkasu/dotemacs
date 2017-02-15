564fddd15c0c80c0a40b2b94c3704caa73435492
Marc A. Kastner <mkastner@fastmail.fm>
2017-02-15 13:48:02 +0900

    Automatic commit at 2017年  2月 15日 水曜日 13:48:02 JST

--- custom.el
+++ custom.el
@@ -3,12 +3,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-quickhelp-color-background "#4e4e4e")
+ '(company-quickhelp-color-foreground "#5fafd7")
+ '(company-quickhelp-delay 0.1)
+ '(company-quickhelp-mode t)
+ '(company-quickhelp-use-propertized-text t)
  '(elpy-modules
    (quote
 	(elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-pyvenv elpy-module-yasnippet elpy-module-sane-defaults)))
+ '(org-agenda-files
+   (quote
+	("/home1/kastnerm/Seafile/org/emacs.org" "/home1/kastnerm/Seafile/org/gm.org" "/home1/kastnerm/Seafile/org/inbox.org" "/home1/kastnerm/Seafile/org/phd-diary.org" "/home1/kastnerm/Seafile/org/test.org")))
  '(package-selected-packages
    (quote
-	(json-mode pkgbuild-mode git-gutter mozc htmlize company go-mode helm helm-core jedi-core markdown-mode projectile python-environment pythonic swiper pcache org helm-projectile use-package org-ehtml org-preview-html framemove deft company-statistics hl-todo highlight-indentation highlight-chars xpm wanderlust undo-tree swiper-helm spaceline rtags robe pyenv-mode-auto popwin paradox org-projectile moe-theme migemo magit jade-mode helm-ag haml-mode go-eldoc glsl-mode flycheck elpy dracula-theme desktop+ darkokai-theme d-mode company-jedi company-irony company-go color-theme-sanityinc-tomorrow cmake-mode cmake-ide clang-format auctex)))
+	(company-quickhelp json-mode pkgbuild-mode git-gutter mozc htmlize company go-mode helm helm-core jedi-core markdown-mode projectile python-environment pythonic swiper pcache org helm-projectile use-package org-ehtml org-preview-html framemove deft company-statistics hl-todo highlight-indentation highlight-chars xpm wanderlust undo-tree swiper-helm spaceline rtags robe pyenv-mode-auto popwin paradox org-projectile moe-theme migemo magit jade-mode helm-ag haml-mode go-eldoc glsl-mode flycheck elpy dracula-theme desktop+ darkokai-theme d-mode company-jedi company-irony company-go color-theme-sanityinc-tomorrow cmake-mode cmake-ide clang-format auctex)))
  '(paradox-github-token t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
